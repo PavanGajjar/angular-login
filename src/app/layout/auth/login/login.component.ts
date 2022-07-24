0b6cf1e5c1adeed3de17c267592ec7f8337311d5
Pavan Gajjar <pavangajjar131298@@gmail.com>
2022-07-24 08:48:27 +0530

    project file added

---
+++ src/app/layout/auth/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup = new FormGroup({});
+  get loginFormControl() { return this.loginForm.controls };
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  createLoginForm() { 
+    this.loginForm = new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.maxLength(10)]),
+      password: new FormControl("", Validators.required)
+    })
+  }
+
+  login(event: any) { 
+    debugger
+  }
+
+  isFieldValid(fieldType: string): boolean { 
+    
+
+    return this.loginFormControl[fieldType].touched ||
+      this.loginFormControl[fieldType].dirty;
+    
+  }
+}
