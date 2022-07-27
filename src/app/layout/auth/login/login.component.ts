9519f0e84b7b6d7019a9a60446bf230eb7a26c9d
Pavan Gajjar <pavangajjar131298@@gmail.com>
2022-07-27 23:21:29 +0530

    Initial commit

--- src/app/layout/auth/login/login.component.ts
+++ src/app/layout/auth/login/login.component.ts
@@ -6,7 +6,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
-
+  
   loginForm: FormGroup = new FormGroup({});
   get loginFormControl() { return this.loginForm.controls };
 
@@ -15,23 +15,24 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.createLoginForm();
   }
-
   createLoginForm() { 
     this.loginForm = new FormGroup({
       email: new FormControl("", [Validators.required, Validators.maxLength(10)]),
       password: new FormControl("", Validators.required)
     })
   }
-
-  login(event: any) { 
-    debugger
+  login() { 
+    if (this.loginForm.valid) {
+      debugger
+    } else {
+      this.showValidationErrors();
+    }
   }
-
   isFieldValid(fieldType: string): boolean { 
-    
-
-    return this.loginFormControl[fieldType].touched ||
-      this.loginFormControl[fieldType].dirty;
+    return (this.loginFormControl[fieldType].touched ||
+      this.loginFormControl[fieldType].dirty) && this.loginFormControl[fieldType].invalid;
+  }
+  showValidationErrors() { 
     
   }
 }
